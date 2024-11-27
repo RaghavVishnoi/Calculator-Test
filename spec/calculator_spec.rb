0f38d6b968c4346bb9422ff39038b63fe06694ec
Raghav Pratap Singh <raghavpratap.singh@zeitview.com>
2024-11-28 02:38:46 +0530

    Step 2: Allowing multiple numbers in a string to add

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe 'AddNumbers' do
     expect(Calculator.new.add(input_two)).to eq(1)
     expect(Calculator.new.add(input_three)).to eq(3)
   end
+
+  it 'allow to add multiple numbers separated by comma' do
+    input_one = '1,2'
+    input_two = '1,3,5,7,8'
+    input_three = '10,23,11,12,10'
+    expect(Calculator.new.add(input_one)).to eq(3)
+    expect(Calculator.new.add(input_two)).to eq(24)
+    expect(Calculator.new.add(input_three)).to eq(66)
+  end
 end
